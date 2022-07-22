712f95ec90617d1887a67433883839b8d901fa9e
Suchanon Jaiwang <suchanon@gopomelo.com>
2022-07-22 17:09:23 +0700

    Add nodemon

--- src/server.js
+++ src/server.js
@@ -17,8 +17,8 @@ const port = 3000;
  })
 
  app.post("/dialogflow", async (req, res) => {
-  const projectId = "jokes-ewit";
-    let sessionId = req.body.ConversationSid;
+  const projectId = "jokes-ewit"; 
+    let sessionId = req.body.ConversationSid;  
     // https://console.twilio.com/us1/service/conversations/IS3b55643eef0b44f681d767a66e5214ca/conversation-service-conversations?frameUrl=%2Fconsole%2Fconversations%2Fservices%2FIS3b55643eef0b44f681d767a66e5214ca%2Fconversations%2FCH95f4a52d6f7042bb8ec7bf4828f08411%3F__override_layout__%3Dembed%26bifrost%3Dtrue%26x-target-region%3Dus1&currentFrameUrl=%2Fconsole%2Fconversations%2Fservices%2FIS3b55643eef0b44f681d767a66e5214ca%2Fconversations%2FCH95f4a52d6f7042bb8ec7bf4828f08411%2Fmessages%3F__override_layout__%3Dembed%26bifrost%3Dtrue%26x-target-region%3Dus1
     let query = req.body.Body;
     // let sessionId = "CH95f4a52d6f7042bb8ec7bf4828f08411"
